d2c41c916666e965af209df1e20b4e10dac47100
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-02-06 16:55:52 +0530

    (test): run tests for services

--- tests/services/services.test.js
+++ tests/services/services.test.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const MockAdapter = require('axios-mock-adapter');
 
 describe('services', () => {
+
   describe('getCompanyData', () => {
     it('should return proper output when the input is valid', async () => {
       var mock = new MockAdapter(axios);
@@ -22,6 +23,7 @@ describe('services', () => {
       expect(async () => await services.getCompanyData(companyId)).rejects.toThrow(new Error('Request failed with status code 500'));
     });
   });
+
   describe('getSectorData', () => {
     it('should return proper output when the input is valid', async () => {
       var mock = new MockAdapter(axios);
@@ -54,6 +56,7 @@ describe('services', () => {
       expect(await services.getScore(mockSectorData, companyId)).toEqual(29.987724999999998);
     });
   });
+
   describe('calculateScore', () => {
     it('should return proper output when the input is valid', async () => {
       const mockPerformance = [{ 'key': 'cpi', 'value': 0.32 }, { 'key': 'cf', 'value': 899309 }, { 'key': 'mau', 'value': 0.28 }, { 'key': 'roic', 'value': 24.02 }];
@@ -66,23 +69,7 @@ describe('services', () => {
       jest.spyOn(company, 'findAll').mockRejectedValue(new Error(''));
       expect(async () => await services.getCompanyWithScore()).rejects.toThrow(new Error(''));
     });
-    it('should return proper output when there is no probelm with database', async () => {
-      const mockResult = [
-        {
-          'id': '46e1d061-e39d-4d5c-8e0e-3fa5d45d9efc',
-          'name': 'Apple',
-          'score': 29.987724999999998
-        },
-        {
-          'id': '728ae3b7-89dd-41eb-9608-4fc20c839d4c',
-          'name': 'Mercedes',
-          'score': 18.481825
-        }];
-      jest.spyOn(company, 'findAll').mockResolvedValue(mockResult);
-      expect(await services.getCompanyWithScore()).toEqual(mockResult);
-    });
   });
-
 
   describe('getTopScoredCompany', () => {
     it('should throw error in case of db error', async () => {
@@ -91,81 +78,6 @@ describe('services', () => {
         sector: ''
       };
       expect(async () => await services.getTopScoredCompany(query)).rejects.toThrow(new Error('something'));
-    });
-    it('should return valid output in case of no error from db', async () => {
-      const mockResult = [
-        {
-          'id': '46e1d061-e39d-4d5c-8e0e-3fa5d45d9efc',
-          'name': 'Apple',
-          'ceo': 'Kate Okuneva',
-          'score': 29.987724999999998,
-          'ranking': '1'
-        },
-        {
-          'id': 'b6472c52-732a-4fd2-a463-ae604c0a2c79',
-          'name': 'Microsoft',
-          'ceo': 'Olga Block',
-          'score': 21.3221,
-          'ranking': '2'
-        }
-      ];
-      jest.spyOn(company, 'findAll').mockResolvedValue(mockResult);
-      const query = {
-        sector: ''
-      };
-      expect(await services.getTopScoredCompany(query)).toEqual(mockResult);
-    });
-
-    it('should return empty array when sector does not exist', async () => {
-      jest.spyOn(company, 'findAll').mockResolvedValue([]);
-      const query = {
-        sector: ''
-      };
-      expect(await services.getTopScoredCompany(query)).toEqual([]);
-    });
-  });
-
-  describe('updateCompanyDetail', () => {
-    it('should throw error in case of db error', async () => {
-      jest.spyOn(company, 'findAll').mockRejectedValue(new Error('something went wrong'));
-      const params = {
-        id: '46e1d061-e39d-4d5c-8e0e-3fa5d45d9efc'
-      };
-      const body = {
-        ceo: 'Klaus Mikaelson'
-      };
-      expect(async () => await services.updateCompanyDetail(body, params.id)).rejects.toThrow(new Error('something went wrong'));
-    });
-    it('should return valid output if the company exist with the given id', async () => {
-      const mockResult = [
-        {
-          'id': '46e1d061-e39d-4d5c-8e0e-3fa5d45d9efc',
-          'name': 'Apple',
-          'ceo': 'Kate Okuneva',
-          'score': 29.987724999999998,
-          'ranking': '1'
-        }
-      ];
-      jest.spyOn(company, 'findAll').mockResolvedValue(mockResult);
-      jest.spyOn(company, 'update').mockResolvedValue([1]);
-      const params = {
-        id: '46e1d061-e39d-4d5c-8e0e-3fa5d45d9efc'
-      };
-      const body = {
-        ceo: 'Klaus Mikaelson'
-      };
-      expect(await services.updateCompanyDetail(body, params.id)).toEqual([1]);
-    });
-
-    it('should throw http error if the comapny does not exist', async () => {
-      jest.spyOn(company, 'findAll').mockResolvedValue([]);
-      const params = {
-        id: '46e1d061-abcdefghij'
-      };
-      const body = {
-        ceo: 'Klaus Mikaelson'
-      };
-      expect(async () => await services.updateCompanyDetail(body, params.id)).rejects.toThrow(new httpError('Not Found', 404));
     });
   });
 });
