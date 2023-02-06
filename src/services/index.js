2243702beef3b729c27bc428cad6e2e15779978f
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-02-06 12:45:21 +0530

    (feat): add services for company data

--- src/services/index.js
+++ src/services/index.js
@@ -1,75 +1,124 @@
+const { company } = require('../../database/models');
+const HttpError = require('../utils/httpError');
 const axios = require('axios');
-const db = require('../../database/models');
+const csvToJson = require('csvtojson');
+const Sequelize = require('sequelize');
 
-const convertCSVtoJSON = (csv) => {
-  const data = csv.split('\n');
-  const result = [];
-  const headers = data[0].split(',');
-  for (let i = 1; i < data.length; i++) {
-    const obj = {};
-    const currentline = data[i].split(',');
-    for (let j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j];
-    }
-    result.push(obj);
+const readCSV = async (urlLink) => {
+  const file = await axios({ method: 'GET', url: urlLink });
+  const jsonData = await csvToJson().fromString(file.data);
+  return jsonData;
+};
+
+const fetchCompanyData = async (jsonData) => {
+  const finalData = [];
+  for (let i = 0; i < jsonData.length; i++) {
+    const { company_id, company_sector } = jsonData[i];
+    const companyData = await getCompanyData(company_id);
+    const companyScore = await getCompanyScores(jsonData[i]);
+    const companyDataWithScore = {
+      id: companyData.id,
+      name: companyData.name,
+      ceo: companyData.ceo,
+      tags: companyData.tags,
+      score: companyScore,
+      sector: company_sector
+    };
+    finalData.push(companyDataWithScore);
   }
+  return finalData;
+};
+
+const getCompanyData = async (companyId) => {
+  const response = await axios({
+    method: 'GET',
+    url: `http://54.167.46.10/company/${companyId}`
+  });
+  return response.data;
+};
+
+const getCompanyScores = async (item) => {
+  const { company_id, company_sector } = item;
+  const sectorData = await getSectorData(company_sector);
+  const score = await getScore(sectorData, company_id);
+  return score;
+};
+
+const getSectorData = async (sectorName) => {
+  const response = await axios({
+    method: 'GET',
+    url: `http://54.167.46.10/sector?name=${sectorName}`
+  });
+  return response.data;
+};
+
+const getScore = async (sectorData, company_id) => {
+  const data = sectorData.filter(element => (element.companyId === company_id));
+  const performance = data[0].performanceIndex;
+  const score = await calculateScore(performance);
+  return score;
+};
+
+const calculateScore = async (performance) => {
+  const result = (((performance[0].value * 10) +
+    (performance[1].value / 10000) +
+    (performance[2].value * 10) +
+    performance[3].value) / 4);
   return result;
 };
 
-const dataDetails = async (url) => {
-  const response = await axios.get(url);
-  const data = convertCSVtoJSON(response.data);
-  const companies = [];
-  const sectors = [];
-  for (const item of data) {
-    const companySpecificDetails = await axios.get(`http://54.167.46.10/company/${item.company_id}`);
-    const company_id = companySpecificDetails.data.id;
-    const name = companySpecificDetails.data.name;
-    const ceo = companySpecificDetails.data.ceo;
-    const companyDetailsFromPost = data.filter((item) => item.company_id === company_id);
-    const sector = companyDetailsFromPost[0].company_sector;
-    const score = 0;
-    const company = { company_id, name, ceo, sector, score };
-    companies.push(company);
+const saveDataToDatabase = async (companyData) => {
+  try {
+    await company.bulkCreate(companyData);
+  } catch (error) {
+    console.log('');
   }
 
-  for (const item of data) {
-    if (!sectors.includes(item.company_sector)) {
-      sectors.push(item.company_sector);
-    }
-  }
-
-  for (const sector of sectors) {
-    const sectorDetails = await axios.get(`http://54.167.46.10/sector?name=${sector}`);
-    const sectorData = sectorDetails.data;
-
-    sectorData.forEach((item) => {
-      const id = item.companyId;
-      const cpi = item.performanceIndex.filter((item) => item.key === 'cpi')[0].value;
-      const cf = item.performanceIndex.filter((item) => item.key === 'cf')[0].value;
-      const mau = item.performanceIndex.filter((item) => item.key === 'mau')[0].value;
-      const roic = item.performanceIndex.filter((item) => item.key === 'roic')[0].value;
-
-      const score = ((cpi * 10) + (cf / 10000) + (mau * 10) + roic) / 4;
-      companies.forEach((company) => {
-        if (company.company_id === id) {
-          company.score = score;
-        }
-      });
-    });
-  }
-  const dbResult = await db.Company.bulkCreate(companies);
-  return dbResult;
 };
 
-const companyData = async (sector) => {
-  const companies = await db.Company.findAll({ where: { sector } }, { order: [['score', 'DESC']] });
+const getCompanyWithScore = async () => {
+  return await company.findAll({
+    attributes: ['id', 'name', 'score']
+  });
+};
+
+const getTopScoredCompany = async (query) => {
+  const companies = await company.findAll({
+    attributes: [
+      'id', 'name', 'ceo', 'score',
+      [Sequelize.literal('(RANK() OVER (ORDER BY score DESC))'), 'ranking']
+    ],
+    where: { sector: query.sector },
+    limit: 2,
+  });
   return companies;
 };
 
-const changeCeo = async (ceo, id) => {
-  const company = await db.Company.update({ ceo }, { where: { id } });
-  return company;
+const updateCompanyDetail = async (body, idGiven) => {
+  const book = await company.findAll({
+    where: { id: idGiven }
+  });
+  if (book.length === 0) {
+    throw new HttpError('Not Found', 404);
+  } else {
+    return await company.update(body, {
+      where: { id: idGiven }
+    });
+  }
 };
 
-module.exports = { dataDetails, companyData, changeCeo };
+
+
+module.exports = {
+  readCSV,
+  updateCompanyDetail,
+  fetchCompanyData,
+  saveDataToDatabase,
+  getCompanyWithScore,
+  getTopScoredCompany,
+  calculateScore,
+  getScore,
+  getSectorData,
+  getCompanyScores,
+  getCompanyData
+};
